a67b53f3c35edc425fd32a45f4d7e614299e40b9
DimTrigkakis <trigkakd@oregonstate.edu>
2017-10-18 06:43:20 -0700

    Initial commit

---
+++ chamber.py
@@ -0,0 +1,14 @@
+class Oracle():
+    # for visualization
+    def __init__(self):
+        pass
+
+class Historian():
+    # for logging
+    def __init__(self):
+        pass
+
+class Chamber():
+    # for the gui with qt
+    def __init__(self):
+        pass
